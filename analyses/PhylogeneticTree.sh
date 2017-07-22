72cf5e67c0cf284259706b3a13173e3903772f0c
Mario Muscarella <mmuscar@illinois.edu>
2017-07-22 12:35:13 -0500

    initial commit

---
+++ analyses/PhylogeneticTree.sh
@@ -0,0 +1,3 @@
+python ../bin/name_change.py TC.bac.final.0.03.rep.fasta TC.bac.final.0.03.rep.rename.fasta
+
+FastTree -gtr -nt -gamma -fastest TC.bac.final.0.03.rep.rename.fasta > TC.bac.rename.tree
