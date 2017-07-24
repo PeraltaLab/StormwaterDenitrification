cb0f90e660b0ee163df9ab43407e55e77cdc652b
Mario Muscarella <mmuscar@illinois.edu>
2017-07-24 15:09:24 -0500

    minor change

--- analyses/PhylogeneticTree.sh
+++ analyses/PhylogeneticTree.sh
@@ -1,3 +1,5 @@
 python ../bin/name_change.py TC.bac.final.0.03.rep.fasta TC.bac.final.0.03.rep.rename.fasta
 
-FastTree -gtr -nt -gamma -fastest TC.bac.final.0.03.rep.fasta > TC.bac.rename.tree
+FastTree -gtr -nt -gamma -fastest TC.bac.final.0.03.rep.fasta > TC.bac.tree
+
+FastTree -gtr -nt -gamma -fastest TC.bac.final.0.03.rep.rename.fasta > TC.bac.rename.tree
